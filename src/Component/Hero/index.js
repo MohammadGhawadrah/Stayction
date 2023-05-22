f1ef83d7b25058ca2102220c070fbd69efc5744a
Mohammad Ghawadrah <mghawadrah@gmail.com>
2023-05-22 21:51:05 +0300

    fixed the hero section style

--- src/Component/Hero/index.js
+++ src/Component/Hero/index.js
@@ -6,25 +6,29 @@ import IconsHomePage from './../IconsHomePage';
 import MainButton from './../MainButton';
 import Typography from '@mui/material/Typography';
 import { createTheme } from "@mui/material/styles";
+import { styled } from '@mui/system';
 
-const theme = createTheme({
-    GridTheme: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around",
-        height: 500,
-    }
+
+
+
+const StyledGrid = styled('div')({
+
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    height: 500,
+
 });
 
 export default function Hero() {
     return (
         <Box position={'relative'}>
             <Grid container spacing={6} columns={{ xs: 12, md: 16 }}>
-                <Grid sx={theme.GridTheme} item xs={12} md={8}>
+                <Grid item xs={12} md={8}><StyledGrid>
                     <Typography variant="h3" color="primary">Forget Busy Work, Start Next Vacation</Typography>
                     <Typography variant="h5" color="thired.main">We provide what you need to enjoy your holiday with family. Time to make another memorable moments.</Typography>
                     <MainButton Text={"Show me More"} />
-                    <IconsHomePage />
+                    <IconsHomePage /></StyledGrid>
                 </Grid>
                 <Grid item xs={12} md={4}>
                     <Image />
