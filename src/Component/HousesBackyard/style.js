280a6aae0c171b0c469e987200aaf71ac2546134
Mohammad Ghawadrah <mghawadrah@gmail.com>
2023-05-30 01:15:22 +0300

    adding api data and delete data.js file and fixing the style

--- src/Component/HousesBackyard/style.js
+++ src/Component/HousesBackyard/style.js
@@ -1,14 +1,10 @@
 import { styled } from '@mui/system';
-
 export const ThemeHouse = styled('div')({
-
-    display: "column",
     justifyContent: "space-between",
     width: 300,
     marginTop: 30,
     marginBottom: 20,
 });
-
 export const StyleHouse = styled('div')({
     marginLeft: -98,
 
